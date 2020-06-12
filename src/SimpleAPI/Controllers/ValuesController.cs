9ae920aa5688e5d33e9d2c4d0b81b2919f9771a7
Vincent Baudry <vbaudry@gmail.com>
2020-06-12 21:57:24 +0200

    Updated API values

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SimpleAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "dotnet", "playbook" };
+            return new string[] { "Vincent", "Baudry" };
         }
 
         // GET api/values/5
