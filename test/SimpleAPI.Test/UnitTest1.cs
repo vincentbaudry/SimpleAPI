de23becda04ba323732ddfa024a3eb7d40aee040
Vincent Baudry <vbaudry@gmail.com>
2020-06-13 08:34:05 +0200

    updated UniTest1.cs

--- test/SimpleAPI.Test/UnitTest1.cs
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -1,10 +1,20 @@
 using System;
 using Xunit;
+using SimpleAPI.Controllers;
 
 namespace SimpleAPI.Test
 {
     public class UnitTest1
     {
+        
+        ValuesController controller = new ValuesController();
+        [Fact]
+        public void GetReturnsCorrectNumber()
+        {
+        var returnValue = controller.Get(1);
+        Assert.Equal("Les Jackson", returnValue.Value);
+        }
+        
         [Fact]
         public void Test1()
         {
